aff4ff762e0962fa07a8394eaaff6b86b2ad0864
Logan Yang <logancyang@gmail.com>
2021-01-16 12:46:10 -0500

    📦  Add __all__ for import

--- loss_landscape_anim/__init__.py
+++ loss_landscape_anim/__init__.py
@@ -1,3 +1,16 @@
 from .main import loss_landscape_anim
+from .model import GenericModel, MLP, LeNet
+from .loss_landscape import LossGrid
+from .datamodule import SpiralsDataModule, MNISTDataModule
 
 __version__ = "0.1.7"
+
+__all__ = [
+    "loss_landscape_anim",
+    "GenericModel",
+    "MLP",
+    "LeNet",
+    "LossGrid",
+    "SpiralsDataModule",
+    "MNISTDataModule",
+]
